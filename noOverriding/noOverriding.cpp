2174987e8e085ca912ab825d56e7f4ab276c9c0f
adilaroisa <adilaroisa688@gmail.com>
2024-05-28 12:11:44 +0700

    membuat class baseclass

--- noOverriding/noOverriding.cpp
+++ noOverriding/noOverriding.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 using namespace std;
 
+//class parent
+//tambahkan final sesudah nama class
+//untuk mencegah adanya overriding(menimpa)
+
+class BaseClass
+{
+public:
+	virtual void perkenalan()  //final (ditambahkan disini jika tidak ingin di override
+	{
+		cout << "halo saya function dari base class";
+	}
+};
+
 int main()
 {
     
